1c01f66dfd73e34060dd6cb3e39ad07b3fc879b8
gamerxdgz <singhgitansh741@gmail.com>
2026-08-01 18:24:01 -0700

    Update browser.js

--- core/browser.js
+++ core/browser.js
@@ -1,1 +1,80 @@
+function openPage(url){
 
+show("browser");
+
+let frame=document.getElementById("page");
+
+document.getElementById("loading")
+.style.display="block";
+
+
+frame.onload=()=>{
+
+document.getElementById("loading")
+.style.display="none";
+
+};
+
+
+frame.src=url;
+
+addHistory(url);
+
+}
+
+
+
+function go(){
+
+let url=document.getElementById("url").value;
+
+
+if(!url.startsWith("http")){
+
+url="https://"+url;
+
+}
+
+
+openPage(url);
+
+}
+
+
+
+function searchWeb(){
+
+let q=document.getElementById("search").value;
+
+openPage(
+searchEngine(
+"DuckDuckGo",
+q
+)
+);
+
+}
+
+
+
+function back(){
+
+page.contentWindow.history.back();
+
+}
+
+
+
+function forward(){
+
+page.contentWindow.history.forward();
+
+}
+
+
+
+function reload(){
+
+page.contentWindow.location.reload();
+
+}
