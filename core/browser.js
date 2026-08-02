84c93145e90e3ff241231ef00ad874ab70f96e96
gamerxdgz <singhgitansh741@gmail.com>
2026-08-01 18:28:41 -0700

    Update browser.js

--- core/browser.js
+++ core/browser.js
@@ -1,22 +1,47 @@
+let currentURL = "";
+
+
 function openPage(url){
+
+currentURL = url;
 
 show("browser");
 
-let frame=document.getElementById("page");
 
-document.getElementById("loading")
-.style.display="block";
+let loader =
+document.getElementById("loading");
 
 
-frame.onload=()=>{
+let frame =
+document.getElementById("page");
 
-document.getElementById("loading")
-.style.display="none";
+
+loader.style.display="block";
+
+
+frame.style.display="none";
+
+
+frame.src=url;
+
+
+
+frame.onload=function(){
+
+loader.style.display="none";
+
+frame.style.display="block";
 
 };
 
 
-frame.src=url;
+
+frame.onerror=function(){
+
+showBlocked(url);
+
+};
+
 
 addHistory(url);
 
@@ -24,9 +49,56 @@ addHistory(url);
 
 
 
+
+function showBlocked(url){
+
+document.getElementById("loading")
+.style.display="none";
+
+
+document.getElementById("page")
+.style.display="block";
+
+
+document.getElementById("page")
+.srcdoc = `
+
+<html>
+
+<body style="
+font-family:Arial;
+text-align:center;
+padding:30px;
+">
+
+<h2>⚠️ Website Blocked</h2>
+
+<p>
+This website does not allow embedding.
+</p>
+
+<p>
+Open it in a real browser engine.
+</p>
+
+<button onclick="window.open('${url}')">
+Open Website
+</button>
+
+</body>
+
+</html>
+
+`;
+
+}
+
+
+
 function go(){
 
-let url=document.getElementById("url").value;
+let url =
+document.getElementById("url").value;
 
 
 if(!url.startsWith("http")){
@@ -44,13 +116,14 @@ openPage(url);
 
 function searchWeb(){
 
-let q=document.getElementById("search").value;
+let q =
+document.getElementById("search").value;
+
 
 openPage(
-searchEngine(
-"DuckDuckGo",
-q
-)
+"https://duckduckgo.com/?q="
++
+encodeURIComponent(q)
 );
 
 }
@@ -59,7 +132,7 @@ q
 
 function back(){
 
-page.contentWindow.history.back();
+history.back();
 
 }
 
@@ -67,7 +140,7 @@ page.contentWindow.history.back();
 
 function forward(){
 
-page.contentWindow.history.forward();
+history.forward();
 
 }
 
@@ -75,6 +148,6 @@ page.contentWindow.history.forward();
 
 function reload(){
 
-page.contentWindow.location.reload();
+openPage(currentURL);
 
 }
