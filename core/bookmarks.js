0e2e90c89f18693c64bc2b48b8dcfd9e6abbfcaf
gamerxdgz <singhgitansh741@gmail.com>
2026-08-01 18:24:52 -0700

    Update bookmarks.js

--- core/bookmarks.js
+++ core/bookmarks.js
@@ -1,1 +1,24 @@
+let bookmarks=[
 
+{
+name:"GitHub",
+url:"https://github.com"
+},
+
+{
+name:"YouTube",
+url:"https://youtube.com"
+}
+
+];
+
+
+function loadBookmarks(){
+
+document.getElementById("bookmarkList")
+.innerHTML =
+bookmarks.map(
+b=>b.name+"<br>"
+).join("");
+
+}
