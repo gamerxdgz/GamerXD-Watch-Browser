5a3b2d7eda0283978ebbb453f9b122771d661dc5
gamerxdgz <singhgitansh741@gmail.com>
2026-08-01 18:24:15 -0700

    Update engines.js

--- core/engines.js
+++ core/engines.js
@@ -1,1 +1,24 @@
+const engines={
 
+Google:
+"https://google.com/search?q=",
+
+Bing:
+"https://bing.com/search?q=",
+
+DuckDuckGo:
+"https://duckduckgo.com/?q=",
+
+Brave:
+"https://search.brave.com/search?q="
+
+};
+
+
+
+function searchEngine(name,text){
+
+return engines[name]+
+encodeURIComponent(text);
+
+}
