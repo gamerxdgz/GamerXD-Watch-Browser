da64613521e5dd861976dcf1fc3430e2f636202e
gamerxdgz <singhgitansh741@gmail.com>
2026-08-01 18:24:32 -0700

    Update history.js

--- core/history.js
+++ core/history.js
@@ -1,1 +1,18 @@
+let historyData=[];
 
+
+function addHistory(url){
+
+historyData.push(url);
+
+}
+
+
+
+function loadHistory(){
+
+document.getElementById("historyList")
+.innerHTML =
+historyData.join("<br>");
+
+}
