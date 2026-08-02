2ea268c23ee1ac3ff1f4b0627736841858017c05
gamerxdgz <singhgitansh741@gmail.com>
2026-08-01 18:08:57 -0700

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,24 +1,61 @@
-function go() {
-    let url = document.getElementById("url").value;
+setTimeout(()=>{
+document.getElementById("loading").style.display="none";
+document.getElementById("app").style.display="block";
+},2000);
 
-    if (!url.startsWith("http")) {
-        url = "https://" + url;
-    }
 
-    window.open(url, "_blank");
+
+function openSite(site){
+
+document.getElementById("home").style.display="none";
+document.getElementById("browser").style.display="block";
+
+document.getElementById("viewer").innerHTML =
+`
+<h3>Opening...</h3>
+<p>${site}</p>
+`;
+
 }
 
 
-function back() {
-    history.back();
+
+function go(){
+
+let url=document.getElementById("url").value;
+
+if(!url.startsWith("http")){
+url="https://"+url;
 }
 
 
-function forward() {
-    history.forward();
+openSite(url);
+
 }
 
 
-function refresh() {
-    location.reload();
+
+function settings(){
+
+document.getElementById("home").style.display="none";
+document.getElementById("settings").style.display="block";
+
+}
+
+
+
+function home(){
+
+document.getElementById("settings").style.display="none";
+document.getElementById("browser").style.display="none";
+document.getElementById("home").style.display="block";
+
+}
+
+
+
+function darkMode(){
+
+document.body.classList.toggle("light");
+
 }
