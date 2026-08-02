8d7d1dffee572bc798e6fa9a134cd581e6d5cafb
gamerxdgz <singhgitansh741@gmail.com>
2026-08-01 18:23:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,70 @@
+setTimeout(()=>{
 
+document.getElementById("boot").style.display="none";
+
+document.getElementById("app").style.display="block";
+
+},2000);
+
+
+
+setInterval(()=>{
+
+let d=new Date();
+
+document.getElementById("clock").innerHTML =
+d.getHours()+":"+d.getMinutes();
+
+},1000);
+
+
+
+function show(section){
+
+document.querySelectorAll("section")
+.forEach(x=>x.style.display="none");
+
+document.getElementById(section)
+.style.display="block";
+
+}
+
+
+function showHome(){
+
+show("home");
+
+}
+
+
+function showBookmarks(){
+
+show("bookmarks");
+
+loadBookmarks();
+
+}
+
+
+function showHistory(){
+
+show("history");
+
+loadHistory();
+
+}
+
+
+function showSettings(){
+
+show("settings");
+
+}
+
+
+
+function toggleTheme(){
+
+document.body.classList.toggle("light");
+
+}
